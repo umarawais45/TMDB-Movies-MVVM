1ca09f6daec07ca15dc1bb35fd0ae34ca04eb42f
Umar Awais <umar.awais@appinsnap.com>
2023-08-26 17:45:38 +0500

    Unit Test Added

--- TMDB-DemoTests/TMDB_DemoTests.swift
+++ TMDB-DemoTests/TMDB_DemoTests.swift
@@ -18,12 +18,20 @@ final class TMDB_DemoTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testFetchMovies() throws {
+        var result: [Movie]?
+        let expectation = self.expectation(description: "Movies Fetched")
+
+        let moviesService = MoviesService()
+        moviesService.fetchMovies() { movies, error in
+            result = movies
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+        XCTAssertNotNil(result)
+        if let result {
+            XCTAssertGreaterThan(result.count, 0)
+        }
     }
 
     func testPerformanceExample() throws {
